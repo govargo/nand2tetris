f581a42b8ea64fedada009706e3d9d0a4a05f08f
Kenta Iso <k-iso@colopl.co.jp>
2020-04-06 23:57:51 +0900

    Fill実装

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,66 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+    @8192
+    D=A
+    @number_of_pixel
+    M=D
+
+    @R0
+    M=0
+(LOOP)
+    @KBD
+    D=M
+    @KEY_PUSHED
+    D;JNE
+(KEY_NOT_PUSHED)
+    @R1
+    M=0
+    @CHECK_STATE_CHANGE
+    0;JMP
+(KEY_PUSHED)
+    @R1
+    M=1
+(CHECK_STATE_CHANGE)
+    @R0
+    D=M
+    @R1
+    D=D-M
+    @LOOP
+    D;JEQ
+    @i
+    M=0
+    @R1
+    D=M
+    @R0
+    M=D
+    @EMPTY_SCREEN_LOOP
+    D;JEQ
+(FILL_SCREEN_LOOP)
+    @i
+    D=M
+    @number_of_pixel
+    D=M-D
+    @LOOP
+    D;JLT
+    @SCREEN
+    A=A+D
+    M=-1
+    @i
+    MD=M+1
+    @FILL_SCREEN_LOOP
+    0;JMP
+(EMPTY_SCREEN_LOOP)
+    @i
+    D=M
+    @number_of_pixel
+    D=M-D
+    @LOOP
+    D;JLT
+    @SCREEN
+    A=A+D
+    M=0
+    @i
+    MD=M+1
+    @EMPTY_SCREEN_LOOP
+    0;JMP
